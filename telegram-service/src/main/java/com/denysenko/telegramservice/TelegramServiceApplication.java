18123dbb3798edcb4b2459e2ea2ca02790f13780
denisenk00 <dmitriy.denisenko.2002@gmail.com>
2024-01-08 17:03:20 +0200

    init commit

---
+++ telegram-service/src/main/java/com/denysenko/telegramservice/TelegramServiceApplication.java
@@ -0,0 +1,15 @@
+package com.denysenko.telegramservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class TelegramServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramServiceApplication.class, args);
+    }
+
+}
