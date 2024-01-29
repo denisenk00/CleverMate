ff6b84c9bee6f1b87f985cc8cfdd1f7b1ab6cee6
denisenk00 <dmitriy.denisenko.2002@gmail.com>
2024-01-29 22:37:47 +0200

    configured circuitbreaker, bulkhead, retry mechanisms

--- message-processor/src/main/java/com/denysenko/messageprocessor/clients/MessageClient.java
+++ message-processor/src/main/java/com/denysenko/messageprocessor/clients/MessageClient.java
@@ -1,10 +1,16 @@
 package com.denysenko.messageprocessor.clients;
 
 import com.denysenko.messageprocessor.dtos.MessageDTO;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient("message-service")
+@CircuitBreaker(name = "botuser-service")
+@Retry(name = "botuser-service")
+@Bulkhead(name = "botuser-service")
 public interface MessageClient {
 
     @PostMapping("/private/messages")
