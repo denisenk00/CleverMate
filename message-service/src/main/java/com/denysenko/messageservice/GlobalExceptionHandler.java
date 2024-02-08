2195a1318bed4925e5c7803c4aa29e23c41048e6
denisenk00 <dmitriy.denisenko.2002@gmail.com>
2024-02-08 15:42:18 +0200

    configured global exception handler

---
+++ message-service/src/main/java/com/denysenko/messageservice/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.denysenko.messageservice;
+
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.denysenko.messageservice.exceptions.JwtNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity handleException(RuntimeException ex){
+        return new ResponseEntity(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler({JwtNotFoundException.class, JWTVerificationException.class})
+    public ResponseEntity handleAuthorizationException(RuntimeException ex){
+        return new ResponseEntity(ex.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+
+}
