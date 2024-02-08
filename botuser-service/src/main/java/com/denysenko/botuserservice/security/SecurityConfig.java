d642a2f8f06be5701d4759d090bf2097c2b4a91f
denisenk00 <dmitriy.denisenko.2002@gmail.com>
2024-02-08 15:14:01 +0200

    fixed the problem when any exceptions evoke AuthorizationException because of redirecting to /error and failing authentication. Hence, authentication process for requests to error dispatcher was disabled

--- botuser-service/src/main/java/com/denysenko/botuserservice/security/SecurityConfig.java
+++ botuser-service/src/main/java/com/denysenko/botuserservice/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.denysenko.botuserservice.security;
 
+import jakarta.servlet.DispatcherType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,23 +19,24 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
-    private final String [] WHITELIST = {};
+    private final String [] WHITELIST = {"/private/**"};
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(CsrfConfigurer::disable)
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers(WHITELIST).permitAll()
-                        .anyRequest().authenticated()
-
-                )
+        return http
+                .csrf(CsrfConfigurer::disable)
+                .cors(CorsConfigurer::disable)
                 .httpBasic(HttpBasicConfigurer::disable)
                 .formLogin(FormLoginConfigurer::disable)
                 .logout(LogoutConfigurer::disable)
+                .authorizeHttpRequests((requests) -> requests
+                        .dispatcherTypeMatchers(DispatcherType.ERROR).permitAll()
+                        .requestMatchers(WHITELIST).permitAll()
+                        .anyRequest().authenticated()
+                )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .addFilterAt(jwtAuthenticationFilter, BasicAuthenticationFilter.class);
-
-        return http.build();
+                .addFilterAt(jwtAuthenticationFilter, BasicAuthenticationFilter.class)
+                .build();
     }
 
 
--- message-service/src/main/java/com/denysenko/messageservice/security/SecurityConfig.java
+++ message-service/src/main/java/com/denysenko/messageservice/security/SecurityConfig.java
@@ -1,15 +1,13 @@
 package com.denysenko.messageservice.security;
 
+import jakarta.servlet.DispatcherType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
-import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.config.annotation.web.configurers.*;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
@@ -21,23 +19,24 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
-    private final String [] WHITELIST = {};
+    private final String [] WHITELIST = {"/private/**"};
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(CsrfConfigurer::disable)
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers(WHITELIST).permitAll()
-                        .anyRequest().authenticated()
-
-                )
+        return http
+                .csrf(CsrfConfigurer::disable)
+                .cors(CorsConfigurer::disable)
                 .httpBasic(HttpBasicConfigurer::disable)
                 .formLogin(FormLoginConfigurer::disable)
                 .logout(LogoutConfigurer::disable)
+                .authorizeHttpRequests((requests) -> requests
+                        .dispatcherTypeMatchers(DispatcherType.ERROR).permitAll()
+                        .requestMatchers(WHITELIST).permitAll()
+                        .anyRequest().authenticated()
+                )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .addFilterAt(jwtAuthenticationFilter, BasicAuthenticationFilter.class);
-
-        return http.build();
+                .addFilterAt(jwtAuthenticationFilter, BasicAuthenticationFilter.class)
+                .build();
     }
 
 
