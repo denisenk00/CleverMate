ae20cf4e1f2c57180f3c97a1a02eee21b6659f36
denisenk00 <dmitriy.denisenko.2002@gmail.com>
2024-02-08 15:15:30 +0200

    added certain exception handlers

--- botuser-service/src/main/java/com/denysenko/botuserservice/GlobalExceptionHandler.java
+++ botuser-service/src/main/java/com/denysenko/botuserservice/GlobalExceptionHandler.java
@@ -1,16 +1,28 @@
 package com.denysenko.botuserservice;
 
 import com.denysenko.botuserservice.exceptions.DocumentNotFoundException;
+import com.denysenko.botuserservice.exceptions.JwtNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(DocumentNotFoundException.class)
     public ResponseEntity handleDocumentNotFoundException(DocumentNotFoundException documentNotFoundException){
         return new ResponseEntity(documentNotFoundException.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(JwtNotFoundException.class)
+    public ResponseEntity handleUnauthorized(Exception e){
+        return new ResponseEntity(e.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity handleInternalServerError(Exception e){
+        return new ResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
